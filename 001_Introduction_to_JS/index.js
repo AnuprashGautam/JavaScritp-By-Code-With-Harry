42c2ca2f4ce0a1e3df73feef723f8044f9d45e00
Anuprash <anuprashgautam@hotmail.com>
2025-06-20 21:43:27 +0530

    Initial files.

---
+++ 001_Introduction_to_JS/index.js
@@ -0,0 +1,8 @@
+console.log("This is the program to add the three numbers:");
+
+function addThreeNumbers(a,b,c){
+  return a+b+c;
+}
+
+let c=addThreeNumbers(1,2,3);
+console.log(c);
---
+++ 003__var_let_and_const_in_JS/index.js
@@ -0,0 +1,29 @@
+console.log("This is about var, let, and const.");
+
+//var
+var a=10;
+var a="Anuprash";
+a=20;
+console.log(a);
+{
+    var a=40;
+    a=50;
+    console.log(a);
+}
+console.log(a);
+
+
+//let
+let y=10;
+// let a="Harry";
+{
+    let y=49;
+    console.log(y);
+}
+z="Harry";
+console.log(z);
+
+
+// const abhi;
+
+
