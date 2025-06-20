96298ff9c75a3d9dc30c129d3e21b10a00e15f9b
Anuprash <anuprashgautam@hotmail.com>
2025-06-21 01:24:25 +0530

    Add initial implementation of while and do-while loops in JavaScript

---
+++ 010 While Loops/index.js
@@ -0,0 +1,17 @@
+// while loop
+/*
+let n = prompt("Enter the value of n")
+n = Number.parseInt(n)
+
+let i = 0;
+while(i<n){
+  console.log(i)
+  i++;
+}
+*/
+
+// do while loop
+
+do{
+  console.log("This loop execute at least once.")
+}while (false) 
