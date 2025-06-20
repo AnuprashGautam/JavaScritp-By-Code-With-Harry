26f2bb84da0f2ea527ab53f42f297184a42be6cc
Anuprash <anuprashgautam@hotmail.com>
2025-06-20 22:32:45 +0530

    Add initial implementation of JavaScript code with string concatenation and object manipulation

---
+++ 005_Chapter-1 Practice Set/index.js
@@ -0,0 +1,38 @@
+// 1
+
+let str="hello"
+let n=9
+
+console.log(str+n)
+
+// 2
+
+console.log(typeof(str+n))
+
+// 3
+
+const a ={
+    "Physics":80,
+    "Chemistry":99,
+    "Maths":90
+}
+
+// a = 100
+
+// 4
+
+a["Chemistry"]=23
+a["Hindi"]=100
+
+// 5
+
+const dictionary={
+    "run":"bagna",
+    "stop":"rukna",
+    "sleep":"sona",
+    "study":"padhna",
+    "play":"khelna"
+}
+
+console.log(dictionary.sleep)
+console.log(dictionary["sleep"])
