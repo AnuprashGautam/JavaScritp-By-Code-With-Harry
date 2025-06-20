8ca4ad596d408a31e0b7d52b58bf248cd8963bab
Anuprash <anuprashgautam@hotmail.com>
2025-06-20 22:18:36 +0530

    Add initial implementation of primitive and non-primitive datatypes

---
+++ 004_Primitives_and_Objects_in_JS/index.js
@@ -0,0 +1,22 @@
+// Premitive Datatypes - NN BB SS U
+
+let a = null
+let b = 123
+let c = true
+let d = BigInt(123) + BigInt(321)
+let e = "Rahul Sharma"
+let f = Symbol("I am Rahul Sharma.")
+let g = undefined
+
+console.log(d)
+
+// Non-premitive Datatypes - Object
+
+const biodata={
+    "name":"Rahul",
+    "married":false,
+    "age":23,
+    "college":"IITB"
+}
+
+console.log(biodata["name"])
