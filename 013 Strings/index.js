86e59bf885d05823d3cf29f9e51808fdff6c6126
Anuprash <anuprashgautam@hotmail.com>
2025-06-21 23:49:40 +0530

    Add initial implementation of string manipulation examples including length, indexing, templates, and escape characters

---
+++ 013 Strings/index.js
@@ -0,0 +1,18 @@
+let string1="THis is the first string."
+let string2='THis is the second string.'
+
+console.log(string1.length)
+console.log(string2[2])
+
+// String Template
+
+let name="Anuprash"
+let template=`I am ${name}.`
+console.log(template)
+
+let temp1=`We can use ' and " inside the string template.`
+console.log(temp1)
+
+// Escape character
+
+console.log("Using \" and \' using the escape character.")
