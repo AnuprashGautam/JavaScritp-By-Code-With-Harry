d528118f58797fac050a2f88ee911b98d8cd505b
Anuprash <anuprashgautam@hotmail.com>
2025-06-22 00:58:23 +0530

    Add complete implementation of array methods including toString, join, pop, push, shift, and unshift

---
+++ 017 Array Methods/index.js
@@ -0,0 +1,36 @@
+let array1=[1,2,3,4,5,6,7, "Anuprash", "Gautam"]
+
+// toString()
+
+let string1=array1.toString()
+console.log(string1)
+
+// join()
+
+let stringJoin=array1.join("_")
+console.log(stringJoin)
+
+// pop()
+
+let n=array1.pop()
+console.log(n)
+console.log(array1)
+
+// push()
+
+let n1=array1.push(100)
+console.log(n1)
+console.log(array1)
+
+// shift()
+
+let n2=array1.shift()
+console.log(n2)
+console.log(array1)
+
+// unshift()
+
+let n3=array1.unshift(101)
+console.log(n3)
+console.log(array1)
+
