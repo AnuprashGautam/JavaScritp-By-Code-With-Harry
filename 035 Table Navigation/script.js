64c2844626f73b0a217299eb4fa7b099cdd257ff
Anuprash <anuprashgautam@hotmail.com>
2025-06-24 23:31:05 +0530

    Add initial HTML structure and JavaScript for table navigation with Bootstrap styling

---
+++ 035 Table Navigation/script.js
@@ -0,0 +1,12 @@
+let t=document.body.firstElementChild.firstElementChild;
+console.log(t)
+console.log(t.rows)
+console.log(t.caption)
+console.log(t.tHead)
+console.log(t.tFoot)
+console.log(t.tFoot)
+console.log(t.tBodies)
+console.log(t.tBodies[0].rows)
+console.log(t.tBodies[0].rows[2].rowIndex)
+console.log(t.tBodies[0].rows[0].cells[2].cellIndex)
+
