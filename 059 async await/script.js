28db4c942af4a24c8b0c61206ec8bbb87fa8cc37
Anuprash <anuprashgautam@hotmail.com>
2025-07-19 00:42:46 +0530

    Added some comments.

--- 059 async await/script.js
+++ 059 async await/script.js
@@ -26,26 +26,26 @@ async function weatherReport(city)
     const api1=`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=4&appid=${apiKey}`;
 
     try{
-        let response1= await fetch(api1);
+        let response1= await fetch(api1);   // Waits for the HTTP request to complete
 
         if(!response1.ok){
             throw new Error(`We are getting improper status from api-1: ${response1.status}`)
         }
 
-        let data1=await response1.json();
+        let data1=await response1.json();   // Waits for JSON parsing to complete
 
         let lat=data1[0].lat;
         let lon=data1[0].lon;
 
         const api2=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
-        let response2= await fetch(api2);
+        let response2= await fetch(api2);   // Waits for the HTTP request to complete
 
         if(!response2.ok){
             throw new Error(`We are getting improper status from api-2: ${response.status}`);
         }
 
-        let data = await response2.json();
+        let data = await response2.json();  // Waits for JSON parsing to complete
         return data;
     }catch(error){
         throw new Error("Failed to fetch weather data: " + error.message);
