710ac0f797d33dd79796112ca2bf0f38ac3aa480
Anuprash <anuprashgautam@hotmail.com>
2025-07-23 01:04:19 +0530

    Completed the hoisting concept.

---
+++ 089 Hoisting in Javascript/script.js
@@ -0,0 +1,27 @@
+greet();
+function greet(){
+    console.log('Hello !!!');
+}
+
+console.log(a);
+
+var a=9;
+
+// console.log(b);
+// const b=9;
+
+// console.log(c);
+// let c=20;
+
+
+// fun();
+// var fun=function(){
+//     console.log('i am not hoisted.');
+// }
+
+// let c1=new Car('Mercedes');
+// var Car=class{
+//     constructor(name){
+//         this.name=name;
+//     }
+// }
