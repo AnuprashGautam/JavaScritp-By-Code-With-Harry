8868eb575b4d33667b875d4435d718e631883bd7
Anuprash <anuprashgautam@hotmail.com>
2025-07-23 02:07:06 +0530

    Completed the clousers.

---
+++ 090 Closures in JS/script.js
@@ -0,0 +1,38 @@
+let name='Oldest';
+
+let outerFunction=function()
+{
+    name='Older';
+    function innerFuction(){
+        console.log(name);
+    }
+    name='Old';
+    return innerFuction;
+}
+
+let c=outerFunction()
+c();
+
+// *******************************************************
+
+function returnFunc() {
+  const x = () => {
+    let a = 1
+    console.log(a)
+    const y = () => {
+      // let a = 2
+      console.log(a)
+      const z = () => {
+        // let a = 3
+        console.log(a)
+      }
+      z()
+    }
+    a = 999
+    y()
+  }
+  return x
+}
+
+let a = returnFunc()
+a()
